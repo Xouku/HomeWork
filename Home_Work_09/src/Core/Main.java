41b67018e4092a408206bccb74856a739600db39
Xouku <57031916+Xouku@users.noreply.github.com>
2021-03-26 16:52:19 +0200

    starting

---
+++ Home_Work_09/src/Core/Main.java
@@ -0,0 +1,23 @@
+package Core;
+
+import java.util.Scanner;
+
+/*
+    -- 09
+    Вводится n чисел. Проверить,
+    что среди них существует ровно два таких числа,
+    что длина (количество цифр) каждого из них равна 3 или 5,
+    а их цифры либо все четные, либо все нечетные.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        while () {
+            if () {
+
+            }
+        }
+    }
+}
